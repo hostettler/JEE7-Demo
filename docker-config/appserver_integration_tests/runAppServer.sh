9f9083f718c885d0ee3c1d60e8d35a14de93991f
Steve Hostettler <steve.hostettler@gmail.com>
2016-03-25 15:42:14 +0100

    Put the integration server to a different set of ports

--- docker-config/appserver_integration_tests/runAppServer.sh
+++ docker-config/appserver_integration_tests/runAppServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d  -p 8080:8080 -p 9990:9990 -p 8787:8787 -v /Users/hostettler/tmp/docker-deploy:/opt/jboss/wildfly/standalone/deployments/:rw jee7-test-wildfly
+docker run -d  -p 8070:8080 -p 9970:9990 -p 8777:8787  jee7-test-wildfly
