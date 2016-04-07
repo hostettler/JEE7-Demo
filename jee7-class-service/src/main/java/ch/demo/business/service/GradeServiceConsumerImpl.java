e6b37884b051b715236d51032697bf5d80d7eb89
Steve Hostettler <steve.hostettler@gmail.com>
2016-04-07 21:48:08 +0200

    first version of the angular 2 interface

--- jee7-class-service/src/main/java/ch/demo/business/service/GradeServiceConsumerImpl.java
+++ jee7-class-service/src/main/java/ch/demo/business/service/GradeServiceConsumerImpl.java
@@ -2,20 +2,14 @@ package ch.demo.business.service;
 
 import java.util.List;
 
-public class GradeServiceConsumerImpl implements GradeServiceConsumer {
+public class GradeServiceConsumerImpl extends AbstractRestConsumer implements GradeServiceConsumer {
 	
-//	private Client client;
-//	private WebTarget target;
-
 	public GradeServiceConsumerImpl() {
-//		client = ClientBuilder.newClient();
-//		target = client.target("http://192.168.99.100:8080/jee7-demo-grade-service/rest/grades/getGradesForStudent");
+		super("http://192.168.99.100:8080/jee7-demo-grade-service/rest/");
 	}
 
 	@Override
-	public Double getAvgGradeForStudents(List<String> students) {
-//		JsonArray response = target.request(MediaType.APPLICATION_JSON).buildPost(students)
-//				.get(JsonArray.class);
-		return 0d;
+	public Double getAvgGradeForStudents(List<String> students) {		
+		return invokeGetWithPathParam("grades/getAvgForStudents/", Double.class, students);
 	}
 }
--- jee7-statics/src/main/webapp/form-validation/src/textarea-d.js
+++
@@ -1,7 +0,0 @@
-angular.module('ngFabForm')
-    .directive('textarea', function ($compile, ngFabFormValidationsDirective)
-    {
-        'use strict';
-
-        return ngFabFormValidationsDirective;
-    });
